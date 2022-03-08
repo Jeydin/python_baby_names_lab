e256f268db2085bf8084034cc5a8b8b3f49a978b
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2022-03-08 02:28:26 +0000

    Finished

--- babyNameFunctions.py
+++ babyNameFunctions.py
@@ -10,8 +10,8 @@ def readBabyNamesFile(year):
 
 def findBabyName(name, year):
 	count = 0
-	deez = readBabyNamesFile(year)
-	for lists in deez:
+	babies = readBabyNamesFile(year)
+	for lists in babies:
 		if lists[0] == name:
 			count += int(lists[2])
 	statement = "There were " + str(count) + " babies named " + name + " in " + year
@@ -34,3 +34,39 @@ def numBabiesInRange(year, beg, end):
     count += int(baby[2])
     statement = "There were " + str(count) + " babies born between " + beg + " and " + end
   return statement
+
+def listInRange(year, min, max):
+  babies = readBabyNamesFile(year)
+  newList = []
+  for baby in babies:
+    if int(baby[2]) < int(max) and int(baby[2]) > int(min):
+      newList.append(baby[0])
+  return newList
+
+def separateGender(year):
+	fn = readBabyNamesFile(year)
+	males = open("yob" + str(year) + "M.txt", 'w')
+	females = open("yob" + str(year) + "F.txt", 'w')
+	for each in fn:
+		if each[1] == "M":
+			print(each[0], file=males)
+		if each[1] == "F":
+			print(each[0], file=females)
+	males.close()
+	females.close()
+	return "Done"
+				
+def createRangeFile(year, beg, end):
+	filename = 'yob' + str(year) + '.txt'
+	file_out = 'yob' + str(year) + 'R' + '.txt'
+	in_file = open(filename, 'r')
+	out_file = open(file_out, 'w')
+	for line in in_file:
+		str_line = line.strip()
+		entry_list = line.split(',')
+		name = entry_list[0]
+		if name[0] == beg and name[-1] == end:
+			print(entry_list[0], file = out_file)
+	in_file.close()
+	out_file.close()
+	return "Done"
--- runner.py
+++ runner.py
@@ -8,3 +8,12 @@ print(findBabyName("Liam", "2020"))
 
 print("====== numBabiesInRange ======")
 print(numBabiesInRange("2020", "Lucy", "Anna"))
+
+# print("====== listInRange ======")
+# print(listInRange("2020", "1", "6"))
+
+print("====== separateGender ======")
+print(separateGender("2020"))
+
+print("====== createRangeFile ======")
+print(createRangeFile("2020", "J", "e"))
